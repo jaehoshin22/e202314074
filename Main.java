ea16a157ce0bd9f40f21ebc0f80f66c2fba9785c
Jaeho, Shin <tlswogh2014@gmail.com>
2026-03-30 03:02:34 +0900

    fifth

--- Main.java
+++ Main.java
@@ -1,2 +1,4 @@
 public class Main { public static void main(String[] args) { System.out.println("신재호");
-world.run() } }
+world.run();
+world.run2();
+ } }
--- World.java
+++ World.java
@@ -1,1 +1,8 @@
-public class World { public static void run() { System.out.println("202314074"); } }
+public class World 
+    { public static void run() { 
+        System.out.println("202314074"); 
+    } 
+    public static void run2() {
+        System.out.println("hello world");
+    }
+}
