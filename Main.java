1b5d7b1a746b99734b1e1c9f03e6b27ff87022dd
Jaeho, Shin <tlswogh2014@gmail.com>
2026-05-13 10:53:40 +0900

    four

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("본인이름");
         World.run();
+        Hello.run();
     }
 }
